37206d946ccc4da91b9c9b21d8cd4409f0b311bd
Namyunha <ajs998@naver.com>
2023-03-14 09:02:33 +0900

    Five

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package git_practice;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Second commit!");
+		System.out.println("Second commit!");
+		System.out.println("Forth commit!");
+		System.out.println("Five commit!");
+	}
+}
